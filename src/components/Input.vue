90c2053f819a24ee853259e21cae1a44dcdcf0cf
余健贤 <929385342@qq.com>
2020-01-15 10:08:20 +0800

    提交修改

--- src/components/Input.vue
+++ src/components/Input.vue
@@ -1,18 +1,38 @@
 <template>
     <div>
-        <input type="text" :value="inputValue" @input="handleInput">
+        <input type="text" v-model="inputValue">
     </div>
 </template>
 
 <script>
     export default {
+        // props 可以是数组也可以是对象 Array<String> || Object
+        // 而对象他就有四个属性。
+        /**
+         * type： 决定他的数据类型。 String ， Number等
+         * default：决定他的默认值
+         * required：false|true 决定他是否必填
+         * validator：验证方法。
+         * */
+        //而我们这里需要用到双向绑定的效果。v-model。
+        //因为刚好这里是input标签，你可以在input标签加入@input输入事件，进行绑定。
+        //但是在有些组件中，你改变的值不会有@input事件。
+        //所以一般我处理是用监听。如下
         props: [
-            'inputValue'
+            'value'
         ],
-        methods: {
-            handleInput (e) {
-                this.$emit("input", e.target.value)
+        data(){
+            return{
+                inputValue:this.value,
             }
+        },
+        watch:{
+            inputValue(val){
+                this.$emit("input", val)
+            },
+            value(val){
+                this.inputValue = val;
+            },
         }
     }
 </script>
